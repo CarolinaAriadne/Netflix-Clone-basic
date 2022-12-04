b3346727b7a5fc1763520584d5cb6db1ba074982
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-12-04 17:20:42 -0300

    finish

--- netflixclone/src/components/MovieTop.js
+++ netflixclone/src/components/MovieTop.js
@@ -4,11 +4,14 @@ import "./MovieTop.css";
 export default ({ item }) => {
   let firstDate = new Date(item.first_air_date);
   let genres = [];
-//   console.log(genres);
   for (let i in item.genres) {
     genres.push(item.genres[i].name);
   }
 
+  let description =  item.overview;
+  // if(description.length > 200){
+  //   description = description.substring(0, 200)+'...';
+  // }
   return (
     <section
       className="featured"
@@ -28,7 +31,7 @@ export default ({ item }) => {
               {item.number_of_seasons} temporada
               {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
-            <div className="featured--description">{item.overview}</div>
+            <div className="featured--description">{description}</div>
             <div className="featured--buttons">
                 <a href={`/watch/${item.id}`} className="featured--watchbutton">▶️  Assistir</a>
                 <a href={`/list/add/${item.id}`} className="featured--mylistbutton" >+ Minha lista</a>
