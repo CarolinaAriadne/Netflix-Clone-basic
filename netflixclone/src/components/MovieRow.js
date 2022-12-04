21ab5971a39eaf3062cf36ceebe7bde6c9e32da5
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-12-04 17:11:28 -0300

    responsividade

--- netflixclone/src/components/MovieRow.js
+++ netflixclone/src/components/MovieRow.js
@@ -4,7 +4,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 
 export default ({ title, items }) => {
-  const [scrollX, setScrollX] = useState(-400);
+  const [scrollX, setScrollX] = useState(0);
 
   const handleLeftArrow = () => {
     let x = scrollX + Math.round(window.innerWidth / 2);
