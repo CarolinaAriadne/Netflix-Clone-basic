0c1f89e559957ccd3e53f8c31ce77336fe9b42fd
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-12-04 16:43:37 -0300

    arrows

--- netflixclone/src/components/MovieRow.js
+++ netflixclone/src/components/MovieRow.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieRow.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listWidth = items.results.length * 150;
+    if (window.innerWidth - listWidth > x) {
+      x = window.innerWidth - listWidth - 60;
+    }
+    setScrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <IoIosArrowForward style={{ fontSize: 30 }} />
+      </div>
+      <div className="movieRow--right" onClick={handleRightArrow}>
+        <IoIosArrowBack style={{ fontSize: 30 }} />
+      </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: items.results.length * 150,
+          }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => {
               return (
                 <div key={key} className="movieRow--item">
-                  <img 
+                  <img
                     src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
                     alt={item.original_title}
                   />
